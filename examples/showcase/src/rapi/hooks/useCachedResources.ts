fbbca806c9602fd2dbbcdae5f1ab0a7838315549
Ed Ryan Alburo <edryanalburo@gmail.com>
2023-11-19 21:18:19 +0800

    Update useCachedResources.ts

--- examples/showcase/src/rapi/hooks/useCachedResources.ts
+++ examples/showcase/src/rapi/hooks/useCachedResources.ts
@@ -1,8 +1,4 @@
 import * as React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-
-import { Asset } from 'expo-asset';
 
 export default function useCachedResources(
 	images?: Array<any> | null,
@@ -15,21 +11,7 @@ export default function useCachedResources(
 		async function loadResourcesAndDataAsync() {
 			try {
 				// Load fonts
-				await Promise.all([
-					images && Asset.loadAsync(images),
-					Font.loadAsync({
-						...Ionicons.font,
-						Ubuntu_300Light: require('../fonts/Ubuntu_300Light.ttf'),
-						Ubuntu_300Light_Italic: require('../fonts/Ubuntu_300Light_Italic.ttf'),
-						Ubuntu_400Regular: require('../fonts/Ubuntu_400Regular.ttf'),
-						Ubuntu_400Regular_Italic: require('../fonts/Ubuntu_400Regular_Italic.ttf'),
-						Ubuntu_500Medium: require('../fonts/Ubuntu_500Medium.ttf'),
-						Ubuntu_500Medium_Italic: require('../fonts/Ubuntu_500Medium_Italic.ttf'),
-						Ubuntu_700Bold: require('../fonts/Ubuntu_700Bold.ttf'),
-						Ubuntu_700Bold_Italic: require('../fonts/Ubuntu_700Bold_Italic.ttf'),
-						...fonts,
-					}),
-				]);
+				// DISABLED FONTS
 			} catch (e) {
 				// We might want to provide this error information to an error reporting service
 				console.warn(e);
