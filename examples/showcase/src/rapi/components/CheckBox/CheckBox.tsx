7ada6587fed7194579de45fa8ac350b5cbcd6203
Ed Ryan Alburo <edryanalburo@gmail.com>
2023-11-19 21:17:11 +0800

    Update CheckBox.tsx

--- examples/showcase/src/rapi/components/CheckBox/CheckBox.tsx
+++ examples/showcase/src/rapi/components/CheckBox/CheckBox.tsx
@@ -8,7 +8,7 @@ import {
   ViewStyle,
 } from "react-native";
 import { component, themeColor } from "../../constants/colors";
-import { Ionicons } from "@expo/vector-icons";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import { useTheme } from "../../provider/ThemeProvider";
 
 interface Props {
